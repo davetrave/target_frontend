1f686bb61bf30056e26116e2a894d16f8463847c
davetrave <daveatmwh@gmail.com>
2024-07-23 23:04:16 +0300

    Reconfigured Deployment Settings by correcting some typo

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const VITE_API_URL = 'http://davemwh.pythonanywhere.com/';
+const VITE_API_URL = 'https://davemwh.pythonanywhere.com/';
 
 const api = axios.create({
   baseURL: VITE_API_URL,
