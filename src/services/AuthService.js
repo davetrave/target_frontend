10ffa5a80edd07fa9725971e3884950b55610868
davetrave <daveatmwh@gmail.com>
2024-07-23 22:45:31 +0300

    Configured Production Settings

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const VITE_API_URL = 'http://localhost:8000/';
+const VITE_API_URL = 'http://davemwh.pythonanywhere.com/';
 
 const api = axios.create({
   baseURL: VITE_API_URL,
